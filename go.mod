209c2651716db78bc0e7b8eb92da82f7000a48e1
Bryan Braun <bbraun7@gmail.com>
2020-07-02 13:29:37 -0400

    hello world ᕕ(ᐛ)ᕗ

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bryanbraun/go-practice
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Visual Studio Code"
+	fmt.Println("Welcome to", name)
+}
